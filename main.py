fd6fc2808e8d4b8b84c4d463b03f29f538136548
ProBook_010 <tzinatullin16@gmail.com>
2022-03-12 16:20:10 +0300

    procfile

--- main.py
+++ main.py
@@ -507,6 +507,6 @@ def not_found(error):
 
 if __name__ == '__main__':
     db_session.global_init("db/blogs.db")
-    # app.register_blueprint(news_api.blueprint)
+    app.register_blueprint(news_api.blueprint)
     port = int(os.environ.get("PORT", 5000))
     serve(app, host='0.0.0.0', port=port)
